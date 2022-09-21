3844a77466e6287f77c6c04936b6182d12be4387
Front Flex <77500425+Piotrko64@users.noreply.github.com>
2022-09-21 21:11:30 +0200

    testing env file in production

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -32,7 +32,7 @@ const Home: NextPage = ({ dataResult }: any) => {
 export default Home;
 
 export async function getStaticProps(context: GetStaticPropsContext) {
-    const dataResult = await fetch("http://localhost:3000/api/githubData")
+    const dataResult = await fetch(`${process.env.URLPAGE}/api/githubData`)
         .then((e) => e.json())
         .then((data) => data);
     return {
