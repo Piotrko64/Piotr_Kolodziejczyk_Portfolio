1a3180dbfc11cea35cf1dc7cb374fa746557c459
Front Flex <77500425+Piotrko64@users.noreply.github.com>
2022-10-01 21:57:28 +0200

    create basic component for image in Cooperation section

--- src/components/homePage/cooperation/subComponents/imageSide/ImageSide.tsx
+++ src/components/homePage/cooperation/subComponents/imageSide/ImageSide.tsx
@@ -1,11 +1,26 @@
 import { HeartSVG } from "./svg/heartSVG/HeartSVG";
 import { PlusSVG } from "./svg/plusSVG/PlusSVG";
+import classes from "./imageSlide.module.css";
+import { NextImage } from "../../../../../ui/images/NextImage";
 
 export function ImageSide() {
     return (
-        <>
-            <HeartSVG />
-            <PlusSVG />
-        </>
+        <div className={classes.container}>
+            <div className={classes.firstLine}>
+                <NextImage
+                    imgURL="/assets/icons/technologies/next.png"
+                    additionalClass={classes.icon}
+                />
+                <PlusSVG />
+                <NextImage
+                    imgURL="/assets/icons/technologies/ts.png"
+                    additionalClass={classes.icon}
+                />
+            </div>
+            <div className={classes.equal}>=</div>
+            <div className={classes.secondLine}>
+                <HeartSVG />
+            </div>
+        </div>
     );
 }
