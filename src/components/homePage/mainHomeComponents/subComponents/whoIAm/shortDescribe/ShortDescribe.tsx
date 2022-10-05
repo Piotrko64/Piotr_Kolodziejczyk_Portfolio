c5667b455316c9522285869d291df17ca93c5ebc
Front Flex <77500425+Piotrko64@users.noreply.github.com>
2022-10-05 11:15:45 +0200

    add translates for first section

--- src/components/homePage/mainHomeComponents/subComponents/whoIAm/shortDescribe/ShortDescribe.tsx
+++ src/components/homePage/mainHomeComponents/subComponents/whoIAm/shortDescribe/ShortDescribe.tsx
@@ -1,10 +1,13 @@
+import useTranslation from "next-translate/useTranslation";
 import classes from "./shortDescribe.module.css";
 
 export function ShortDescribe() {
+    const { t } = useTranslation("home");
     return (
         <p className={classes.describe}>
-            Moją pasją jest <span className="boldViolet"> tworzenie stron </span> oraz
-            <span className="boldRed"> aplikacji internetowych </span>
+            {t("myPassion")}
+            <span className="boldViolet"> {t("createPage")} </span> {t("and")}
+            <span className="boldRed"> {t("apps")} </span>
         </p>
     );
 }
