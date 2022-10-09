70aef5430675695fd0a420704bc6d3bc012af765
Front Flex <77500425+Piotrko64@users.noreply.github.com>
2022-10-09 14:03:27 +0200

    change revalidate

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -60,6 +60,6 @@ export const getStaticProps: GetStaticProps = async ({ locale }) => {
                 locale as Language
             ),
         },
-        revalidate: 72000,
+        revalidate: 84_000,
     };
 };
