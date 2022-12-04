c44bad0dbf14de4b183f20aa4ddc525e43cb8467
Front Flex <77500425+Piotrko64@users.noreply.github.com>
2022-12-04 13:46:00 +0100

    Fix problem with translate

--- src/components/homePage/about/subComponents/textSide/TextSide.tsx
+++ src/components/homePage/about/subComponents/textSide/TextSide.tsx
@@ -2,7 +2,7 @@ import useTranslation from "next-translate/useTranslation";
 import classes from "./textSide.module.css";
 
 export function TextSide() {
-    const { t } = useTranslation("coop");
+    const { t } = useTranslation("about");
 
     return <div className={classes.text}>{t("text")}</div>;
 }
