bed50720d7b56a171bd906ff153eac0ee966df00
Front Flex <77500425+Piotrko64@users.noreply.github.com>
2022-12-04 18:39:04 +0100

    Fix path to styles css

--- src/components/homePage/cooperation/subComponents/textSide/TextSide.tsx
+++ src/components/homePage/cooperation/subComponents/textSide/TextSide.tsx
@@ -1,5 +1,5 @@
 import useTranslation from "next-translate/useTranslation";
-import classes from "./textSide.module.css";
+import classes from "./TextSide.module.css";
 
 export function TextSide() {
     const { t } = useTranslation("coop");
