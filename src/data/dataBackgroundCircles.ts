c2aab73d91d771f85461d2da64b5445e215fb329
Front Flex <77500425+Piotrko64@users.noreply.github.com>
2022-12-15 20:07:01 +0100

    Add styles for footer

--- src/data/dataBackgroundCircles.ts
+++ src/data/dataBackgroundCircles.ts
@@ -1,5 +1,5 @@
 export const dataBackgroundCircles = [
     { name: "red", strenth: 0.42 },
     { name: "yellow", strenth: 0.2 },
-    { name: "violet", strenth: 0.4 },
+    { name: "violet", strenth: 0.5 },
 ];
