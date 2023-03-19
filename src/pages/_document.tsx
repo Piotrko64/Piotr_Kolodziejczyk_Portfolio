32c120b01409a154933d9bc47b8f6dcdef22a62f
Front Flex <77500425+Piotrko64@users.noreply.github.com>
2023-03-19 11:14:49 +0100

    ✨feature: Add google seacrh console to meta tag

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -11,29 +11,33 @@ export default function Document() {
                     content="PK | Portfolio"
                 ></meta>
                 <meta
+                    name="google-site-verification"
+                    content="OxpD9r8hicE3Tn3gSMnJnzfaDhVtsF8L-baBE628jHo"
+                />
+                <meta
                     name="apple-mobile-web-app-status-bar-style"
                     content="black"
-                ></meta>
+                />
                 <meta name="theme-color" content="#1d1c2d" />
                 <link
                     rel="apple-touch-icon"
                     href="/assets/icons/manifest/icon48x48.png"
                     sizes="48x48"
-                ></link>
+                />
                 <link
                     rel="apple-touch-icon"
                     href="/assets/icons/manifest/icon144x144.png"
                     sizes="144x144"
-                ></link>
+                />
                 <link
                     href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700;900&display=swap"
                     rel="stylesheet"
-                ></link>
+                />
                 <link
                     href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap"
                     rel="stylesheet"
-                ></link>
-                <link rel="shortcut icon" href="/favicon.ico"></link>
+                />
+                <link rel="shortcut icon" href="/favicon.ico" />
             </Head>
             <body>
                 <Main />
