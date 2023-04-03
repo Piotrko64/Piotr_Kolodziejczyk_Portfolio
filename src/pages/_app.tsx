531f98a4c89eaec47e3edfb5eb09e16e9cd7c9c5
Front Flex <77500425+Piotrko64@users.noreply.github.com>
2023-04-03 20:45:21 +0200

    ✨feature: Add GoogleAnalytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,12 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { Analytics } from "@vercel/analytics/react";
+import { GoogleAnalytics } from "nextjs-google-analytics";
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
         <>
+            <GoogleAnalytics trackPageViews />
             <Component {...pageProps} /> <Analytics />
         </>
     );
