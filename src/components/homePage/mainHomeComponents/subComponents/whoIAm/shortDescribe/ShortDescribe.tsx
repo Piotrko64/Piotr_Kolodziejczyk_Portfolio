5eb130f34d45818ec520a3aa882b4e8e676bf634
Front Flex <77500425+Piotrko64@users.noreply.github.com>
2023-08-27 21:41:55 +0200

    Change main description

--- src/components/homePage/mainHomeComponents/subComponents/whoIAm/shortDescribe/ShortDescribe.tsx
+++ src/components/homePage/mainHomeComponents/subComponents/whoIAm/shortDescribe/ShortDescribe.tsx
@@ -1,13 +1,5 @@
-import useTranslation from "next-translate/useTranslation";
 import classes from "./shortDescribe.module.css";
 
 export function ShortDescribe() {
-    const { t } = useTranslation("home");
-    return (
-        <p className={classes.describe}>
-            {t("myPassion")}
-            <span className=""> {t("createPage")} </span> {t("and")}
-            <span className=""> {t("apps")} </span>
-        </p>
-    );
+    return <p className={classes.describe}>Frontend Developer</p>;
 }
