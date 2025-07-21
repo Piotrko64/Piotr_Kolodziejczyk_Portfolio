ab5abc8ba6cdfd8cf7bb7a4bb9a723ba81466b66
Piotr Kołodziejczyk <77500425+Piotrko64@users.noreply.github.com>
2025-07-21 09:46:31 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const nextTranslate = require("next-translate");
 
 module.exports = nextTranslate({
     images: {
-        domains: ["media.graphassets.com"],
+        domains: ["media.graphassets.com", "eu-central-1-shared-euc1-02.graphassets.com", "eu-west-2.graphassets.com"],
     },
     eslint: {
         ignoreDuringBuilds: true,
